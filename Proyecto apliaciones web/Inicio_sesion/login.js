8e8d8b7d9a464087d6c70b1505322792ee7c4603
MiguelCh34 <144953713+MiguelCh34@users.noreply.github.com>
2024-05-12 00:20:23 -0500

    Add files via upload

---
+++ Proyecto apliaciones web/Inicio_sesion/login.js
@@ -0,0 +1,15 @@
+function loguear()
+{
+    let user=document.getElementById("Usuario").value;
+    let pass=document.getElementById("Contraseña").value;
+
+    if(user=="Juan" && "1234")
+        {
+           window.location="../Pagina_principal/Pagina_principal.html";
+        }
+
+    else
+        {
+            alert("Datos incorrectos");
+        }
+}
